5ca39d7c3d0516dd875451cd0ac6c6ad9ec3765a
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-09-13 12:50:46 +0530

    Add files via upload

---
+++ Introdcution_Java_Activity/MyProgram.java
@@ -0,0 +1,9 @@
+/*
+@Author Sankhajit Roy
+*/
+
+public class MyProgram {    // main class
+    public static void main(String arg[]){    // main method
+        System.out.println("My first java program...");    // print the statement
+    }
+}
